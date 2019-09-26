692d08dfab5396bf4f8804d1e1841753b2975825
Yu-Chun Lin <y.lin38@ncl.ac.uk>
2019-09-26 11:37:49 +0800

    Updates

---
+++ precache-manifest.c221119e2da1d35f452bda94a88fb6fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd663b277a2f835f3c4f9d0b9e5b7696",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "8bc1168ca043f1b842b2",
+    "url": "/resume/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8613faa26e176252c5ea",
+    "url": "/resume/static/js/2.bd25d8e3.chunk.js"
+  },
+  {
+    "revision": "8bc1168ca043f1b842b2",
+    "url": "/resume/static/js/main.af6a03f3.chunk.js"
+  },
+  {
+    "revision": "6d9f940cbe3dd9586d1c",
+    "url": "/resume/static/js/runtime-main.db1178c1.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/resume/static/media/logo.25bf045c.svg"
+  }
+]);
